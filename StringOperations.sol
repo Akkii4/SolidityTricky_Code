a35966baa2d0c61e658cebc516f00c893c28bebf
Akshit Choudhary <akshitchoudhary007@gmail.com>
2022-01-21 00:11:36 +0530

    Create StringOperations.sol

---
+++ StringOperations.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: TOO_BUSY_FOR_COURT
+pragma solidity ^0.8.0;
+
+contract StringOperations {
+    function getStringLength(string calldata s) public pure returns (uint) {
+        return bytes(s).length;
+    }
+
+    function concatenateStrings(string calldata a, string calldata b) public pure returns (string memory) {
+        return string(abi.encodePacked(a,b));
+    }
+
+    function reverseStrings(string calldata s) public pure returns (string memory) {
+        bytes memory b = bytes(s);
+        string memory r = new string(b.length);
+        bytes memory rb = bytes(r);
+        for(uint i = 0; i < b.length; i++) {
+            rb[i] = b[b.length - i - 1];
+        }
+        return string(rb);
+    }
+}
