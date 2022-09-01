f5517a00de18814d5d9a0569c47e4f048394a558
Akshit Choudhary <akshitchoudhary007@gmail.com>
2022-09-01 17:40:08 +0530

    fixed document formatting

--- CreateContract/CreateContract.sol
+++ CreateContract/CreateContract.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-
-import './Test.sol';
+import "./Test.sol";
 
 contract CreateContract {
-
     event CreatedNew(address _contractAddress);
     event Create(address _contractAddress);
     event Create2(address _contractAddress);
@@ -17,9 +15,9 @@ contract CreateContract {
     }
 
     // Get creation bytecode of the contract
-    function getBytecode(uint _testVar) public pure returns (bytes memory){
+    function getBytecode(uint256 _testVar) public pure returns (bytes memory) {
         bytes memory code = type(Test).creationCode;
-        return abi.encodePacked(code,abi.encode(_testVar)); // encode the bytecode with the constructor parameter
+        return abi.encodePacked(code, abi.encode(_testVar)); // encode the bytecode with the constructor parameter
     }
 
     // useful to determine any changes to the contract's bytecode caused due to change in the contract
@@ -28,17 +26,18 @@ contract CreateContract {
     }
 
     // 2. Create contract by create method
-    function createBytecode(bytes memory bytecode) external payable{
+    function createBytecode(bytes memory bytecode) external payable {
         address addr;
         assembly {
             // create(v, p, n)
             // v= amount of ETH to send
             // p = pointer in memory to start of code, (0x20 is 32 in hex)
             // n = size of code
-            addr := create(callvalue(), // wei value sending as part of the call
-                           add(bytecode, 0x20), // add 32 bytes to start of code, as first 32 bytes are length of code
-                           mload(bytecode) // size of code stored in first 32 bytes
-                        )
+            addr := create(
+                callvalue(), // wei value sending as part of the call
+                add(bytecode, 0x20), // add 32 bytes to start of code, as first 32 bytes are length of code
+                mload(bytecode) // size of code stored in first 32 bytes
+            )
         }
         require(addr != address(0), "creation failed");
         emit Create(addr);
@@ -47,7 +46,11 @@ contract CreateContract {
     //   Compute the address of the contract to be deployed through create2 method
     //   keccak256(0xff + deployer address + salt + keccak256 (creation code))
     //   take last 20 bytes
-    function preComputeCreate2Address (bytes memory bytecode, uint _salt) public view returns (address) {
+    function preComputeCreate2Address(bytes memory bytecode, uint256 _salt)
+        public
+        view
+        returns (address)
+    {
         bytes32 hash = keccak256(
             abi.encodePacked(
                 bytes1(0xff),
@@ -60,30 +63,29 @@ contract CreateContract {
     }
 
     // 3. Create contract by create2 method
-    function create2Bytecode(bytes memory bytecode, uint _salt) external payable{
+    function create2Bytecode(bytes memory bytecode, uint256 _salt)
+        external
+        payable
+    {
         address addr;
         assembly {
             // create(v, p, n, s)
             // v= amount of ETH to send
             // p = pointer in memory to start of code
             // n = size of code
-            // s = salt (any arbitrary value for randomness) 
-            addr := create2(callvalue(),
-                            add(bytecode, 32), 
-                            mload(bytecode), 
-                            _salt // salt
-                        )
             // s = salt (any arbitrary value for randomness)
             addr := create2(
                 callvalue(), // wei sent with current call
                 add(bytecode, 32), // Actual code starts after skipping the first 32 bytes
                 mload(bytecode), // load the size of code
+                _salt // salt
+            )
 
-            if iszero(extcodesize(addr)) {  // if contract size is empty, revert
-                revert(0,0)
+            if iszero(extcodesize(addr)) {
+                // if contract size is empty, revert
+                revert(0, 0)
             }
         }
-        emit Create2(addr);  
+        emit Create2(addr);
     }
-
 }
