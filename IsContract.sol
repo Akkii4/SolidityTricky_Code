a2241bb0e68e4ce9a59b76440916c8a88aecd1cb
Akshit Choudhary <akshitchoudhary007@gmail.com>
2023-07-05 13:57:06 +0530

    Update IsContract.sol

--- IsContract.sol
+++ IsContract.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.8.0;
 
-   /* You shouldn't rely on `isContract` to protect against flash loan attacks!
+   /* You shouldn't rely on `isContract` (to check if the call is made only be EOA and not by contract) to protect against flash loan attacks!
     *
     * Preventing calls from contracts is highly discouraged. It breaks composability, breaks support for smart wallets
     * like Gnosis Safe, and does not provide security since it can be circumvented by calling from a contract
@@ -10,15 +10,13 @@ pragma solidity 0.8.0;
 contract CodeSize {
 
     function isContract_new(address account) public view returns (bool) {
-        // This method relies on extcodesize/address.code.length, which returns 0
-        // for contracts in construction, since the code is only stored at the end
-        // of the constructor execution.
+        // This method returns 0 for contracts in construction, since the code is only stored at the end of the constructor execution.
         return account.code.length > 0;
     }
 
     function isContract_old(address _addr) public view returns (bool){
         uint256 size;
-        // retrieve the size of the code, this needs assembly
+        // retrieve the size of the code, this needs assembly, can be bypassed if used during constructor
         assembly {
             size := extcodesize(_addr)
         }
